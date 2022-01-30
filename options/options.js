a05024c74d3df723257523a1973d614051314bdf
Crowdbotics <team@crowdbotics.com>
2022-01-30 21:39:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "epp_recycling_bin_33252",
+  url: "https://epp_recycling_bin_33252.botics.co",
+  api: "https://epp_recycling_bin_33252.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
